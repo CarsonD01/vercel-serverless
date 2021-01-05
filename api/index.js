1495c0be8f2efe0e8af8754d00934ad76e921a2c
Ricardo Olarte <raop155@hotmail.com>
2021-01-05 18:55:37 -0500

    Install express & mongoose deps

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-module.exports = (req, res) => {
-  res.send('Hello World');
-};
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('*', (req, res) => {
+  res.send('Happy Piggy');
+});
+
+module.exports = app;
