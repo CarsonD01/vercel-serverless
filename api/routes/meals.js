4f1e55bbbe79d3ac974cd5bc13616992038af72e
Ricardo Olarte <raop155@hotmail.com>
2021-01-06 14:29:32 -0500

    Add CRUD to API

--- api/routes/meals.js
+++ api/routes/meals.js
@@ -1,13 +1,34 @@
 const express = require('express');
-
-const router = app.router();
+const Meals = require('../models/Meals');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('GET meals');
+  Meals.find()
+    .exec()
+    .then((x) => res.status(200).json(x));
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  Meals.findById(id)
+    .exec()
+    .then((x) => res.status(200).json(x));
 });
 
 router.post('/', (req, res) => {
-  res.send('POST meals');
+  const body = req.body;
+  Meals.create(body).then((x) => res.status(201).json(x));
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  Meals.findOneAndUpdate(id, body).then(() => res.sendStatus(204));
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  Meals.findOneAndDelete(id).then(() => res.sendStatus(204));
 });
 
 module.exports = router;
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,13 +1,34 @@
 const express = require('express');
-
-const router = app.router();
+const Orders = require('../models/Orders');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('GET orders');
+  Orders.find()
+    .exec()
+    .then((x) => res.status(200).json(x));
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  Orders.findById(id)
+    .exec()
+    .then((x) => res.status(200).json(x));
 });
 
 router.post('/', (req, res) => {
-  res.send('POST orders');
+  const body = req.body;
+  Orders.create(body).then((x) => res.status(201).json(x));
+});
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  Orders.findOneAndUpdate(id, body).then(() => res.sendStatus(204));
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  Orders.findOneAndDelete(id).then(() => res.sendStatus(204));
 });
 
 module.exports = router;
