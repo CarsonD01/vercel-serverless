4d6ce4e69ef9b9f09f3667dd50b8f2e3e8f765b1
Ricardo Olarte <raop155@hotmail.com>
2021-01-06 17:54:35 -0500

    Create Users Schema

--- api/routes/meals.js
+++ api/routes/meals.js
@@ -3,21 +3,17 @@ const Meals = require('../models/Meals');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  Meals.find()
-    .exec()
-    .then((x) => res.status(200).json(x));
+  Meals.find().then((x) => res.status(200).send(x));
 });
 
 router.get('/:id', (req, res) => {
   const id = req.params.id;
-  Meals.findById(id)
-    .exec()
-    .then((x) => res.status(200).json(x));
+  Meals.findById(id).then((x) => res.status(200).send(x));
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  Meals.create(body).then((x) => res.status(201).json(x));
+  Meals.create(body).then((x) => res.status(201).send(x));
 });
 
 router.put('/:id', (req, res) => {
