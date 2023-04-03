7b8c1845b2b21513f58aa85ca0b0881606aa34ee
carsond <85541094+CarsonD01@users.noreply.github.com>
2023-04-03 16:43:06 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,22 +1,11 @@
 const express = require('express');
-//const mongoose = require('mongoose');
-const cors = require('cors');
-//const meals = require('./routes/meals');
-//const orders = require('./routes/orders');
-//const users = require('./routes/users');
-//const auth = require('./routes/auth');
+//const cors = require('cors');
 const auth = require('./routes/test');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
-//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-//app.use('/api/meals', meals);
-//app.use('/api/orders', orders);
-//app.use('/api/users', users);
-//app.use('/api/auth', auth);
 app.use('/api/test', test);
 
 module.exports = app;
