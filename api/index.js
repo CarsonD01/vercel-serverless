7ff64c957d5610251f53b9521bf7bf6e155cf5df
carsond <85541094+CarsonD01@users.noreply.github.com>
2023-04-03 16:39:40 +0800

    Update index.js
    for test

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const cors = require('cors');
-const meals = require('./routes/meals');
-const orders = require('./routes/orders');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+//const meals = require('./routes/meals');
+//const orders = require('./routes/orders');
+//const users = require('./routes/users');
+//const auth = require('./routes/auth');
 const auth = require('./routes/test');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.use('/api/meals', meals);
-app.use('/api/orders', orders);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
+//app.use('/api/meals', meals);
+//app.use('/api/orders', orders);
+//app.use('/api/users', users);
+//app.use('/api/auth', auth);
 app.use('/api/test', test);
 
 module.exports = app;
